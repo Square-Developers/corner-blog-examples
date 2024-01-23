69e5d80bbedb696aa1726d74936e247423b1a79c
Anthony Giuliano <agiuliano@squareup.com>
2024-01-23 11:58:03 -0600

    Revert "removing route handler implementation"
    This reverts commit a7b42c1b7ecf1c4f88458b2a456927928e6c661b.

--- squaredev-next-online-payments-example/src/app/page.js
+++ squaredev-next-online-payments-example/src/app/page.js
@@ -4,13 +4,29 @@ import { submitPayment } from "./actions/actions";
 
 export default function Home() {
   // Replace with your application ID and location ID
-  const appId = "sandbox-sq0idb-2PCGtqlNn89i-sHMmRY5HQ";
-  const locationId = "LFJW4DZVPHVRR";
+  const appId = "YOUR_APP_ID";
+  const locationId = "YOUR_LOCATION_ID";
 
+  // Below we have provided both the Route Handler and Server Actions implementations
+  // Remove the implementation that you are not using
   return (
     <PaymentForm
       applicationId={appId}
       locationId={locationId}
+      // This is the Route Handler implementation
+      cardTokenizeResponseReceived={async (token, verifiedBuyer) => {
+        const response = await fetch("/api/pay", {
+          method: "POST",
+          headers: {
+            "Content-type": "application/json",
+          },
+          body: JSON.stringify({
+            sourceId: token.token,
+          }),
+        });
+        console.log(await response.json());
+      }}
+      // This is the Server Actions implementation
       cardTokenizeResponseReceived={async (token) => {
         const result = await submitPayment(token.token);
         console.log(result);
